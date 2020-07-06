fe6cc34b294c3f1e41f8421a12cfbbad2c612385
鵜瀬郁美 <choco.08193@gmail.com>
2020-07-06 22:14:34 +0900

    変数とデータ型
    変数とデータ型

---
+++ 2-2/index.php
@@ -0,0 +1,57 @@
+<?php
+// index.php
+$message = "Hello World!";
+echo $message;
+
+//Hello World!を出力したあとに改行
+echo "<br>";
+
+$x = 8;
+$y = 5.55;
+echo $x;
+
+//8を出力したあとに改行
+echo "<br>";
+echo $y;
+echo "<br>";
+?>
+
+<?php
+// index.php
+$message = "Hello World!";
+var_dump($message);
+
+//Hello World!を出力したあとに改行
+echo "<br>";
+
+$x = 8;
+$y = 5.55;
+var_dump($x);
+
+//8を出力したあとに改行
+echo "<br>";
+var_dump($y);
+
+echo "<br>";
+$flag = true;
+var_dump($flag);
+
+echo "<br>";
+$emp = null;
+var_dump($emp);
+echo "<br><br>";
+
+//課題
+echo "【API】";
+echo "<br>";
+echo "自己のソフトウェアの一部をWEB上に公開することによって、誰でも外部から利用することができるようになる。";
+echo "<br>";
+echo "それによって、自分のソフトウェアに他のソフトウェアの機能を埋め込むことができるようになるので、アプリケーション同士で連携することが可能になる。";
+echo "<br>";
+echo "<br>";
+echo "【SVN(SVN(Subversion】";
+echo "<br>";
+echo "オープンソースのバージョン管理システムの一つ。複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントを管理するのに用いられる。";
+echo "<br>";
+echo "ファイルの変更履歴を管理してくれるシステムの一つ";
+?>
